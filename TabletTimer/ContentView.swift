f6be2e9545c1ec785f07cb0d5b171f7db9d0a665
Daniel Pape <danielwpape@gmail.com>
2020-05-09 16:15:26 +0100

    Initial Commit

---
+++ TabletTimer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TabletTimer
+//
+//  Created by Daniel Pape on 09/05/2020.
+//  Copyright © 2020 Daniel Pape. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
